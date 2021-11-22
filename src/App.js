528eb9f2f54d11cd3c63098a21b74bffd607484c
kaindarkwind <74018069+KainDarkwind@users.noreply.github.com>
2021-11-21 23:49:41 -0500

    Shell Page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React from "react";
+import "./style/master.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/pages/Landing";
+import AllCards from "./components/pages/AllCards";
+import CreateAnswer from "./components/pages/CreateAnswer";
+import CreateImagery from "./components/pages/CreateImagery";
+import Edit from "./components/pages/Edit";
+import ReviewAnswer from "./components/pages/ReviewAnswer";
+import ReviewEmpty from "./components/pages/ReviewEmpty";
+import ReviewImagery from "./components/pages/ReviewImagery";
+
+import NotFound from "./components/pages/NotFound";
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-danger">hello world</h1>
-    </div>
-  );
+   return (
+      <Router>
+         <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/create-answer" element={<CreateAnswer />} />
+            <Route path="/create-imagery" element={<CreateImagery />} />
+            <Route path="/review-imagery" element={<ReviewImagery />} />
+            <Route path="/review-answer" element={<ReviewAnswer />} />
+            <Route path="/review-answer" element={<ReviewEmpty />} />
+            <Route path="/all-cards" element={<AllCards />} />
+            <Route path="/edit" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+         </Routes>
+      </Router>
+   );
 }
 
 export default App;
